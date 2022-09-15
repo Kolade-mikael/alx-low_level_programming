9c9600a96013ded5a295fbdf6cac47ed1482e9bb
Kolade Akande <akandemichael215@gmail.com>
2022-09-15 14:32:08 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests functiom that prints if integer is positive or negative
- * Retun: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
